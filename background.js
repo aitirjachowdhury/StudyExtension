f5fd207b868043d37a47717f1ad9038a7bbcbd7f
Aiti <aitichowdhury5@gmail.com>
2021-05-02 13:56:58 -0400

    CSS changes

--- background.js
+++ background.js
@@ -1,3 +1,29 @@
-chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
-});
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.local.get(["blocked", "enabled"], function (local) {
+      if (!Array.isArray(local.blocked)) {
+        chrome.storage.local.set({ blocked: [] });
+      }
+  
+      if (typeof local.enabled !== "boolean") {
+        chrome.storage.local.set({ enabled: false });
+      }
+    });
+  });
+  
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+    const url = changeInfo.pendingUrl || changeInfo.url;
+    if (!url || !url.startsWith("http")) {
+        return;
+      }
+    
+      const hostname = new URL(url).hostname;
+    
+      chrome.storage.local.get(["blocked", "enabled"], function (local) {
+        const { blocked, enabled } = local;
+        if (Array.isArray(blocked) && enabled && blocked.find(domain => hostname.includes(domain))) {
+            alert("You are reaching for a Forbbiden Site :( \nDon't get distracted!");
+            chrome.tabs.remove(tabId);
+        }
+      });
+    });
